42828bac5e79499d2d0a61562fe3228aa49fe983
Majid-Ebrahimi <majidebrahiimi@gmail.com>
2023-09-06 20:52:08 +0330

    Fix more than 1 operator calculations

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,11 @@ import MainCalculator from "./components/layouts/MainCalculator";
 
 import CalculatorNumber from "./components/ui/CalculatorNumber";
 
-let resultTemp: number
+let calculationResult: number
 let firstValue: number
-let tempOperator: string
+let operatorCalculation: string
+let moreOperator: boolean = false
+
 
 function App() {
     const [value, setValue] = useState("0")
@@ -19,6 +21,9 @@ function App() {
         multiplication = "*",
         division = "/",
     }
+
+    let isIncludesOperator = (value.includes(operator.addition) || value.includes(operator.subtraction) || value.includes(operator.multiplication) || value.includes(operator.division))
+    let isEndsWithOperator = (value.endsWith(operator.addition) || value.endsWith(operator.subtraction) || value.endsWith(operator.multiplication) || value.endsWith(operator.division))
 
     function setValueNumber(num: string) {
 
@@ -37,26 +42,48 @@ function App() {
     }
 
     function operatorHandler(opr: string) {
-        if (!(value.endsWith(operator.addition) || value.endsWith(operator.subtraction) || value.endsWith(operator.multiplication) || value.endsWith(operator.division))) {
-            setValue(value.concat(opr))
-            firstValue = parseInt(value)
-            tempOperator = opr
+        if (!isEndsWithOperator) {
+            if (!isIncludesOperator) {
+                setValue(value.concat(opr))
+                firstValue = parseFloat(value)
+                operatorCalculation = opr
+                moreOperator = false
+            } else {
+                moreOperator = true
+                resultHandler(opr)
+            }
         }
     }
 
-    function resultHandler() {
+    function resultHandler(opr?: string) {
 
-        const secondValue = parseInt(value.substring(value.indexOf(tempOperator) + 1, value.length))
-        if (tempOperator === operator.addition) {
-            resultTemp = firstValue + secondValue
-        } else if (tempOperator === operator.subtraction) {
-            resultTemp = firstValue - secondValue
-        } else if (tempOperator === operator.multiplication) {
-            resultTemp = firstValue * secondValue
-        } else if (tempOperator === operator.division) {
-            resultTemp = firstValue / secondValue
+        if (!isEndsWithOperator) {
+            if (isIncludesOperator) {
+                const secondValue = parseFloat(value.substring(value.indexOf(operatorCalculation) + 1, value.length))
+                if (operatorCalculation === operator.addition) {
+                    calculationResult = firstValue + secondValue
+                } else if (operatorCalculation === operator.subtraction) {
+                    calculationResult = firstValue - secondValue
+                } else if (operatorCalculation === operator.multiplication) {
+                    calculationResult = firstValue * secondValue
+                } else if (operatorCalculation === operator.division) {
+                    calculationResult = firstValue / secondValue
+                }
+                if (moreOperator) {
+                    if (opr != null) {
+                        operatorCalculation = opr
+                    }
+                    setValue(calculationResult.toString().concat(operatorCalculation))
+                    moreOperator = false
+                    firstValue = calculationResult
+                } else {
+                    setValue(calculationResult.toString())
+                    if (opr != null) {
+                        operatorCalculation = opr
+                    }
+                }
+            }
         }
-        setValue(resultTemp.toString())
 
     }
 
